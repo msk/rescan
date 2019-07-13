5b516478eca858837ce477dff9e99c0e014807b6
Min Kim <msk@dolbo.net>
2019-07-13 14:53:29 +0900

    Parse a string and a sequence of characters
    No regular expression syntax has been implemented yet.

--- src/parser.rs
+++ src/parser.rs
@@ -1,13 +1,36 @@
 mod control_verbs;
 mod parser_util;
 
+use nom::{bytes::complete::take, IResult};
+
 use crate::parser::control_verbs::read_control_verbs;
-use crate::util::compile_error::{CompileError, ErrorKind};
+use crate::util::compile_error::CompileError;
 pub(crate) use parser_util::ParseMode;
 
 pub(crate) fn parse(ptr: &str, global_mode: &mut ParseMode) -> Result<(), CompileError> {
     let p = ptr;
 
-    let _p = read_control_verbs(p, 0, global_mode)?;
-    Err(CompileError::new(ErrorKind::Other, "not implemented"))
+    let mut p = read_control_verbs(p, 0, global_mode)?;
+
+    while !p.is_empty() {
+        if let Ok((input, _c)) = take_any(p) {
+            p = input
+        }
+    }
+    Ok(())
+}
+
+fn take_any(input: &str) -> IResult<&str, &str> {
+    take(1usize)(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_any() {
+        let mut mode = ParseMode::default();
+        assert!(parse("abc", &mut mode).is_ok());
+    }
 }
