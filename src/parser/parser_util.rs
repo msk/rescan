2af16aa251d385cc6d354615d6477fef02e1ea79
Min Kim <msk@dolbo.net>
2019-11-01 21:12:32 -0700

    Add `dotall`, `ignore_space`, and `multiline` to `ParseMode`

--- src/parser/parser_util.rs
+++ src/parser/parser_util.rs
@@ -3,6 +3,9 @@ use crate::Flags;
 #[derive(Clone, Copy, Debug, Default)]
 pub(crate) struct ParseMode {
     pub(crate) caseless: bool,
+    pub(crate) dotall: bool,
+    pub(crate) ignore_space: bool,
+    pub(crate) multiline: bool,
     pub(crate) ucp: bool,
     pub(crate) utf8: bool,
 }
@@ -11,6 +14,9 @@ impl ParseMode {
     pub(crate) fn new(flags: Flags) -> Self {
         Self {
             caseless: flags.contains(Flags::CASELESS),
+            dotall: flags.contains(Flags::DOTALL),
+            ignore_space: false,
+            multiline: flags.contains(Flags::MULTILINE),
             ucp: flags.contains(Flags::UCP),
             utf8: flags.contains(Flags::UTF8),
         }
